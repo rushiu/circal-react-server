885c8fadbbe03f98d5d92807269caa918d3b893e
natasharao <rao117@purdue.edu>
2020-06-16 17:19:54 -0700

    Started sign up flow and adjusted spacing and started dashboard

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -8,12 +8,19 @@ import '.././App.css';
 import '.././css/signup.css';
 
 export class SignUp extends React.Component {
+    continue = e => {
+        e.preventDefault();
+        this.props.nextStep();
+    };
     render() {
+
+        const {values, inputChange} = this.props;
+
         return(
             <Container className = "font" fluid>
                 <SimpleNav/>
                 <Container className = "sign-up-bg animate__animated animate__slideInRight" fluid>
-                    <Table className = "animate__animated animate__fadeIn">
+                    <Table className = "animate__animated animate__slideInLeft">
                         <Row>
                             <h1 className = "title header mx-auto welcome-header">Welcome Back!</h1>
                         </Row>
@@ -40,22 +47,22 @@ export class SignUp extends React.Component {
                             <Col>
                                 <Form className = "mx-auto" style = {{width: '60%'}}>
                                     <Form.Row>
-                                        <Form.Control type = "full name" placeholder = "Full Name"/>
+                                        <Form.Control type = "first name" placeholder = "First Name" onChange = {inputChange('firstName')} value = {values.firstName}/>
                                     </Form.Row>
-                                    <Form.Row className = "padding">
-                                        <Form.Control type = "work email" placeholder = "Work Email"/>
+                                    <Form.Row className = "pt-3">
+                                        <Form.Control type = "last name" placeholder = "Last Name" onChange = {inputChange('lastName')} value = {values.lastName}/>
                                     </Form.Row>
-                                    <Form.Row className = "padding">
-                                        <Form.Control type = "company" placeholder = "Company Name"></Form.Control>
+                                    <Form.Row className = "pt-3">
+                                        <Form.Control type = "work email" placeholder = "Work Email" onChange = {inputChange('email')} value = {values.email}/>
                                     </Form.Row>
-                                    <Form.Row className = "padding">
-                                        <Form.Control type = "password" placeholder = "Password"></Form.Control>
+                                    <Form.Row className = "pt-3">
+                                        <Form.Control type = "company" placeholder = "Company Name" onChange = {inputChange('company')} value = {values.company}/>
                                     </Form.Row>
-                                    <Form.Row className = "padding">
+                                    <Form.Row className = "pt-3">
                                         <Form.Check type="checkbox" label="Agree to Terms of Service"/>
                                     </Form.Row>
-                                    <Form.Row className = "padding">
-                                        <Button variant="primary" style = {{border: 'hidden', width: '40%', fontWeight: 'bold', fontSize: '1.2rem'}} size = "lg" className = "btn-rounded mx-auto">sign up</Button>
+                                    <Form.Row className = "pt-3">
+                                        <Button variant="primary" style = {{border: 'hidden', width: '40%', fontWeight: 'bold', fontSize: '1.2rem'}} size = "lg" className = "btn-rounded mx-auto" onClick = {this.continue}>sign up</Button>
                                     </Form.Row>
                                 </Form>
                             </Col>
