5ec6cbede38dd58fd6b986af7dc0786c75817644
natasharao <rao117@purdue.edu>
2020-06-17 10:25:40 -0700

    merging

--- src/components/User/SideNavigation.js
+++ src/components/User/SideNavigation.js
@@ -5,27 +5,26 @@ import './dashboard.css'
 import { faSmile } from '@fortawesome/free-solid-svg-icons';
 
 const SideBarStyle = {
-    position: 'fixed',
+    position: 'absolute',
     left: '0', 
     bottom: '0',
-    height: 'calc(100vh - 90px)',
-    width: '230px',
+    width: '13%',
     position: 'fixed',
     top: '0',
     left: '0',
-    zIndex: '1',
+    zIndex: '0',
     minHeight: '100%',
     fontSize: '1.5rem',
     background: 'linear-gradient(141.68deg, #FAD7A1 -27.76%, #FD6585 103.99%)',
-    boxShadow: '0px 2px 10px rgba(0, 0, 0, 0.10)'
+    boxShadow: '0px 2px 10px rgba(0, 0, 0, 0.10)',
+    zIndex: '0'
 }
 
 export class SideNavigation extends React.Component {
     render(){
         return(
-            <Container className = "font" style = {{zIndex: '3', position: 'fixed'}}fluid>
                 <Navbar variant="dark"  expand="lg" style = {SideBarStyle}>
-                    <Container style = {{position: 'absolute', top: '2%', paddingRight: '1%'}}>
+                    {/* <Container style = {{position: 'absolute', top: '2%', paddingRight: '1%'}}>
                         <img
                         alt=""
                         src="/assests/LogoImage.png"
@@ -34,7 +33,7 @@ export class SideNavigation extends React.Component {
                         height="50"
                         />
                         <Navbar.Brand className = "font" variant = "dark" style = {{left: '30%', position: 'absolute'}}><h1>circal</h1></Navbar.Brand>
-                    </Container>
+                    </Container> */}
                     <Row style = {{position: 'absolute', left: '1.5rem', right: '0', justifyContent: 'center', display: 'flex'}}>
                         <Nav className = "flex-column">
                             <Nav.Link href = "/user_dashboard"><h5 className = "center-nav-link">home</h5></Nav.Link>
@@ -49,7 +48,6 @@ export class SideNavigation extends React.Component {
                         </Col>
                     </Row>
                 </Navbar>
-            </Container>
         );
     }
 }
