f4173258ab26c376d89fc37227d6756b3f2c5da8
natasharao <rao117@purdue.edu>
2020-06-17 12:08:16 -0700

    fixing issues

--- src/App.js
+++ src/App.js
@@ -10,18 +10,10 @@ import {Pricing} from './components/Pricing.js';
 import {NavigationBar} from './components/NavigationBar.js';
 import { BrowserRouter, Router, Route, Switch} from 'react-router-dom';
 import { SignUp } from './components/SignUp';
-<<<<<<< HEAD
 import {SignInNext} from './components/SignInNext.js';
 import {Password} from './components/Password.js';
-<<<<<<< HEAD
 import { PersonalInfo } from './components/PersonalInfo';
 import {SignIn} from './components/SignIn.js';
-=======
-=======
-import { PersonalInfo } from './components/PersonalInfo';
-import {SignIn} from './components/SignIn.js';
->>>>>>> Started sign up flow and adjusted spacing and started dashboard
->>>>>>> Started sign up flow and adjusted spacing and started dashboard
 import CalendarPicker from './components/CalendarPicker';
 import UserDashboard from './components/User/UserDashboard';
 import SignUpNext from './components/SignUpNext';
